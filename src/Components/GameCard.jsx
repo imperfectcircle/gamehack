89e913c8bcd0b767623683e739837af50444570d
Fabio Angelici <fabio.angelici@outlook.it>
2023-09-20 17:49:18 +0200

    Modifiche minori di stile

--- src/Components/GameCard.jsx
+++ src/Components/GameCard.jsx
@@ -5,7 +5,7 @@ export default function GameCard({ data, className = '' }) {
         <Link to={`/game/${data.id}`}>
             <div
                 className={`
-                group relative h-[300px] overflow-hidden rounded-lg rounded-l-none rounded-t-none border-b-2 border-r-2 bg-white p-10 shadow-lg transition-all duration-500 hover:scale-105 hover:bg-orange-500 dark:border-none dark:bg-gray-400 dark:hover:bg-orange-700 ${className}`}
+                group relative h-[300px] rounded-lg rounded-l-none rounded-t-none border-b-2 border-r-2 bg-white p-10 shadow-lg transition-all duration-500 hover:scale-105 hover:bg-orange-500 dark:border-none dark:bg-gray-400 dark:hover:bg-orange-700 ${className}`}
             >
                 <p className="mb-5 text-center text-lg group-hover:text-white">
                     {data.name}
@@ -14,7 +14,7 @@ export default function GameCard({ data, className = '' }) {
                 <img
                     src={data.background_image}
                     alt={`${data.name} cover picture`}
-                    className="h-36 w-full rounded-br-lg object-cover transition-all duration-500 group-hover:scale-[1.2]"
+                    className="h-36 w-full rounded-br-lg object-cover transition-all duration-500 group-hover:scale-[1.1]"
                 />
             </div>
         </Link>
